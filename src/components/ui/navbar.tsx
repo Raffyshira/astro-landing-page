cac99c9ec1f23ce8a9d7891747f74f9bba93c1f0
Raffyshira <ahsira88prayoga@gmail.com>
2024-09-13 08:04:22 +0700

    deleted animate

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -151,7 +151,13 @@ export const SheetMobileNav = ({
                     </ScrollArea>
                     <SheetFooter className="border-b">
                         <div className="w-full flex flex-col gap-2 mb-2">
-                            <Button aria-label="sidebar button" className="w-full font-semibold" asChild>
+                            <Button
+                                role="button"
+                                id="sidebar-button"
+                                aria-label="sidebar-button"
+                                className="w-full font-semibold"
+                                asChild
+                            >
                                 <a href="/">Login</a>
                             </Button>
                             <Button
